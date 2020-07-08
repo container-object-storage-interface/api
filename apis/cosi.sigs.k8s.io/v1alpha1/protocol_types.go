af284c1279a84628ccf5582c537535eba87fabd9
jcope <jcope@redhat.com>
2020-07-08 12:37:52 -0500

    updated go type APIs
    moved genereated CRDs to config/crds simplify dir tree
    
    added kubebuilder defaulter, validation tags
    
    added api approval annotation

--- apis/cosi.sigs.k8s.io/v1alpha1/protocol_types.go
+++ apis/cosi.sigs.k8s.io/v1alpha1/protocol_types.go
@@ -5,16 +5,16 @@ type ProtocolSignature string
 const (
 	ProtocolSignatureS3    ProtocolSignature = "s3"
 	ProtocolSignatureAzure ProtocolSignature = "azure"
-	ProtocolSignatureGCS    ProtocolSignature = "gcs"
+	ProtocolSignatureGCS   ProtocolSignature = "gcs"
 )
 
 type Protocol struct {
 	// +kubebuilder:validation:Enum:={s3,azure,gcs}
 	ProtocolSignature ProtocolSignature `json:"protocolSignature"`
 	// +optional
-	S3                S3Protocol        `json:"s3,omitempty"`
+	S3 S3Protocol `json:"s3,omitempty"`
 	// +optional
-	Azure             AzureProtocol     `json:"azure,omitempty"`
+	Azure AzureProtocol `json:"azure,omitempty"`
 	// +optional
-	GCS               GCSProtocol       `json:"gcs,omitempty"`
+	GCS GCSProtocol `json:"gcs,omitempty"`
 }
