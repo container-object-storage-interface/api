cbaf0bc835b02fb780dc18d9820cd4000c7b0273
Srini Brahmaroutu <srbrahma@us.ibm.com>
2020-09-19 00:06:23 -0700

    moving api group to storage v1alpha1

--- clientset/fake/clientset_generated.go
+++ clientset/fake/clientset_generated.go
@@ -20,8 +20,8 @@ package fake
 
 import (
 	clientset "github.com/container-object-storage-interface/api/clientset"
-	objectstoragev1alpha1 "github.com/container-object-storage-interface/api/clientset/typed/objectstorage.k8s.io/v1alpha1"
-	fakeobjectstoragev1alpha1 "github.com/container-object-storage-interface/api/clientset/typed/objectstorage.k8s.io/v1alpha1/fake"
+	objectstoragev1alpha1 "github.com/container-object-storage-interface/api/clientset/typed/storage.k8s.io/v1alpha1"
+	fakeobjectstoragev1alpha1 "github.com/container-object-storage-interface/api/clientset/typed/storage.k8s.io/v1alpha1/fake"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/discovery"
--- informers/externalversions/generic.go
+++ informers/externalversions/generic.go
@@ -21,7 +21,7 @@ package externalversions
 import (
 	"fmt"
 
-	v1alpha1 "github.com/container-object-storage-interface/api/apis/objectstorage.k8s.io/v1alpha1"
+	v1alpha1 "github.com/container-object-storage-interface/api/apis/storage.k8s.io/v1alpha1"
 	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	cache "k8s.io/client-go/tools/cache"
 )
